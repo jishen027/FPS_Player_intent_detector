a1a9cbf998bf8840594be9ec51020f00d1445110
J Li <jli306@sheffield.ac.uk>
2022-08-16 23:45:01 +0100

    [ADD] firestore config

---
+++ vite-three/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from './.firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyClz8_9nwl6l5ReK4c44h_ajsDxYuf_VTw",
+  authDomain: "three-js-demo-58da4.firebaseapp.com",
+  projectId: "three-js-demo-58da4",
+  storageBucket: "three-js-demo-58da4.appspot.com",
+  messagingSenderId: "273050549481",
+  appId: "1:273050549481:web:c54af289a4a30ea7cb4478"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const colRef = collection(db, 'coordinateData');
+
+export { app, db, colRef };
