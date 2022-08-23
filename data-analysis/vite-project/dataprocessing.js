63ed0141c30779ee3cab1544cd0777b16b877fc5
J Li <jli306@sheffield.ac.uk>
2022-08-23 06:53:45 +0100

    [ADD] ipynb for build machine model

---
+++ data-analysis/vite-project/dataprocessing.js
@@ -0,0 +1,33 @@
+function isMSbrowser() {
+  const  userAgent = window.navigator.userAgent
+  return userAgent.indexOf('Edge') !== -1 || userAgent.indexOf('Trident') !== -1
+}
+
+function format(data) {
+  return String(data).replace(/"/g, '""').replace(/(^[\s\S]*$)/, '"$1"')
+}
+
+export function saveCSV(title, head, data) {
+  let wordSeparator = ','
+  let lineSeparator = '\n'
+
+  let reTitle = title + '.csv'
+  let headBOM = '\ufeff'
+  let headStr = head ? head.map(item => format(item)).join(wordSeparator) + lineSeparator : ''
+  let dataStr = data ? data.map(row => row.map(item => format(item)).join(wordSeparator)).join(lineSeparator) : '';
+  // let dataStr = data ? data.map(row => row.ArrayType + wordSeparator + row.data + lineSeparator) : '';
+
+  return isMSbrowser()
+    ? new Promise(resolve => { // Edge、IE11
+      let blob = new Blob([headBOM + headStr + dataStr], { type: 'text/plain;charset=utf-8' })
+      window.navigator.msSaveBlob(blob, reTitle)
+      resolve()
+    })
+    : new Promise(resolve => { // Chrome、Firefox
+      let a = document.createElement('a')
+      a.href = 'data:text/csv;charset=utf-8,' + headBOM + encodeURIComponent(headStr + dataStr)
+      a.download = reTitle
+      a.click()
+      resolve()
+    })
+}
--- data-analysis/vite-project/main.js
+++ data-analysis/vite-project/main.js
@@ -2,13 +2,16 @@ import * as echart from 'echarts';
 
 import { getDocs } from 'firebase/firestore';
 import { colRef } from './firebase-config'
+import { saveCSV } from './dataprocessing';
 
 //get Data from firestore
 
 var myChart = echart.init(document.getElementById('main'));
 var myChart2 = echart.init(document.getElementById('main2'));
+var myChart3 = echart.init(document.getElementById('main3'));
+var myChart4 = echart.init(document.getElementById('main4'));
 
-function getData(type, myChart) {
+function getData(type, chartOne, ChartTwo, chartTitle) {
   let data = [];
   let axis = [];
   let axes = [];
@@ -18,40 +21,77 @@ function getData(type, myChart) {
       snapshot.forEach((doc) => {
         data.push(doc.data());
       })
-      console.log(data)
-      let type_1 = data.filter((doc) => { return doc.ArrayType == type}).slice(0, 100);
+      let type_1 = data.filter((doc) => { return doc.ArrayType == type })
       type_1.forEach((d) => {
         d.data.forEach((a) => {
-          axis.push(a.x);
           axis.push(a.y);
+          axis.push(a.x);
           axes.push(axis);
           axis = []
         })
         Type_1_data.push(axes);
         axes = [];
       })
-      console.log(Type_1_data);
       let coordinateSeries = [];
+      console.log(Type_1_data);
       Type_1_data.forEach((data) => {
         coordinateSeries.push({
-          type: 'scatter',
+          symbol: 'none',
+          type: 'line',
           data: data
         })
       })
       console.log(coordinateSeries);
 
-      setGraphOption(coordinateSeries, myChart);
+      setGraphOption(coordinateSeries, chartOne, chartTitle);
+
+      // cetre point
+      let centerpoints = [];
+      let centrePoint = [];
+      Type_1_data.forEach((coordinates) => {
+        var x = 0, y = 0;
+        for (let i = 1; i < coordinates.length; i++) {
+          const axesX = +coordinates[i][0].toFixed(2)
+          const axesY = +coordinates[i][1].toFixed(2)
+          x = x + axesX
+          y = y + axesY
+        }
+        centrePoint.push(x);
+        centrePoint.push(y);
+        centerpoints.push(centrePoint);
+
+        //init
+        centrePoint = [];
+        x = 0, y = 0;
+      })
+      console.log(centerpoints);
+      let centerPointSeries = [
+        {
+          type: 'scatter',
+          data: centerpoints
+        }
+      ];
+      setGraphOption(centerPointSeries, ChartTwo, "Center Points")
+
+      // output CSV
+      let csvTitle = 'center point csv ' + type;
+      let head = ['x', 'y'];
+      let csvData = centerpoints;
+
+      saveCSV(csvTitle, head, csvData).then(() => {
+        console.log('success');
+      })
     })
     .catch(err => {
       console.log(err.message);
     })
 }
 
-function setGraphOption(coordinateSeries, chart) {
-  console.log(coordinateSeries)
+//set option
+function setGraphOption(coordinateSeries, chart, chartTitle) {
   chart.setOption({
     title: {
-      text: 'ECharts Getting Started Example'
+      text: chartTitle
     },
     xAxis: {},
     yAxis: {},
@@ -60,8 +100,10 @@ function setGraphOption(coordinateSeries, chart) {
 }
 
 // Aiming action
-getData(0, myChart);
+getData(0, myChart, myChart3, 'Aiming Action Samples');
 
 // moving action
-getData(1, myChart2);
+getData(1, myChart2, myChart4, 'Moving Action Samples');
+
+
 
