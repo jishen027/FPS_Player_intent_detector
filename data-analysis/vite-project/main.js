252dfc49823828a99b6355b165afc7ca69d5fb52
J Li <jli306@sheffield.ac.uk>
2022-08-25 12:21:46 +0100

    [UPDATE] new data collection methods

--- data-analysis/vite-project/main.js
+++ data-analysis/vite-project/main.js
@@ -11,72 +11,101 @@ var myChart2 = echart.init(document.getElementById('main2'));
 var myChart3 = echart.init(document.getElementById('main3'));
 var myChart4 = echart.init(document.getElementById('main4'));
 
-function getData(type, chartOne, ChartTwo, chartTitle) {
+function getData() {
+  let collection = [];
   let data = [];
-  let axis = [];
-  let axes = [];
-  let Type_1_data = [];
+  let line = [];
+  let point = [];
+
+  let mata_0 = [];
+  let mata_1 = [];
+  let centerPoint = [];
+  let lineCenterPoints = [];
+
+  let total_x = 0, total_y = 0, total_rx = 0, total_ry = 0;
+  let total_x_0 = 0, total_y_0 = 0, total_rx_0 = 0, total_ry_0 = 0;
+  let counter_0 = 0, counter_1 = 0;
+
+
+
   getDocs(colRef)
     .then((snapshot) => {
       snapshot.forEach((doc) => {
-        data.push(doc.data());
+        data.push(doc.data())
       })
-      let type_1 = data.filter((doc) => { return doc.ArrayType == type })
-      type_1.forEach((d) => {
-        d.data.forEach((a) => {
-          axis.push(a.y);
-          axis.push(a.x);
-          axes.push(axis);
-          axis = []
+      data.forEach((elements) => {
+        elements.data.forEach((element, index) => {
+          point.push(element.x)
+          point.push(element.y)
+          point.push(element.rx)
+          point.push(element.ry)
+          point.push(element.trigger)
+          line.push(point)
+          point = [];
+
+
+          if (element.trigger > 0) {
+            total_x += element.x;
+            total_y += element.y;
+            total_rx += element.rx;
+            total_ry += element.ry;
+            counter_1++;
+          } else {
+            total_x_0 += element.x;
+            total_y_0 += element.y;
+            total_rx_0 += element.rx;
+            total_ry_0 += element.ry;
+            counter_0++;
+          }
+
         })
-        Type_1_data.push(axes);
-        axes = [];
-      })
-      let coordinateSeries = [];
-      console.log(Type_1_data);
-      Type_1_data.forEach((data) => {
-        coordinateSeries.push({
-          symbol: 'none',
-          type: 'line',
-          data: data
-        })
-      })
-      console.log(coordinateSeries);
+        collection.push(line)
+        line = [];
 
-      setGraphOption(coordinateSeries, chartOne, chartTitle);
-
-      // cetre point
-      let centerpoints = [];
-      let centrePoint = [];
-      Type_1_data.forEach((coordinates) => {
-        var x = 0, y = 0;
-        for (let i = 1; i < coordinates.length; i++) {
-          const axesX = +coordinates[i][0].toFixed(2)
-          const axesY = +coordinates[i][1].toFixed(2)
-          x = x + axesX
-          y = y + axesY
-        }
-        centrePoint.push(x);
-        centrePoint.push(y);
-        centerpoints.push(centrePoint);
-
-        //init
-        centrePoint = [];
-        x = 0, y = 0;
+        mata_0.push(total_x_0 / counter_0);
+        mata_0.push(total_y_0 / counter_0);
+        mata_0.push(total_rx_0 / counter_0);
+        mata_0.push(total_ry_0 / counter_0);
+        mata_1.push(total_x / counter_1);
+        mata_1.push(total_y / counter_1);
+        mata_1.push(total_rx / counter_1);
+        mata_1.push(total_ry / counter_1);
+        centerPoint.push(mata_0);
+        centerPoint.push(mata_1);
+        lineCenterPoints.push(centerPoint);
+        mata_0 = [];
+        mata_1 = [];
+        centerPoint = [];
       })
-      console.log(centerpoints);
-      let centerPointSeries = [
-        {
-          type: 'scatter',
-          data: centerpoints
-        }
-      ];
-      setGraphOption(centerPointSeries, ChartTwo, "Center Points")
+      console.log(collection);
+      console.log(lineCenterPoints);
+
+      let collection_points = []
+      let col_point = []
+
+      lineCenterPoints.forEach((element) => {
+        col_point.push(element[0][0])
+        col_point.push(element[0][1])
+        col_point.push(element[0][2])
+        col_point.push(element[0][3])
+        col_point.push(0)
+        collection_points.push(col_point)
+        col_point = []
+        col_point.push(element[1][0])
+        col_point.push(element[1][1])
+        col_point.push(element[1][2])
+        col_point.push(element[1][3])
+        col_point.push(1)
+        collection_points.push(col_point)
+        col_point = []
+      })
+
+      console.log(collection_points);
 
       // output CSV
-      let csvTitle = 'center point csv ' + type;
-      let head = ['x', 'y'];
-      let csvData = centerpoints;
+      let csvTitle = 'ControllerData';
+      let head = ['x','y','rx','ry','type'];
+      let csvData = collection_points;
 
       saveCSV(csvTitle, head, csvData).then(() => {
         console.log('success');
@@ -103,11 +132,9 @@ const fetchData = document.getElementById('FetchData');
 fetchData.addEventListener('click', (e) => {
   e.preventDefault();
   // Aiming action
-  getData(0, myChart, myChart3, 'Aiming Action Samples');
+  getData();
 
   // moving action
-  getData(1, myChart2, myChart4, 'Moving Action Samples');
+  // getData(1, myChart2, myChart4, 'Moving Action Samples');
 });
-
-
 
